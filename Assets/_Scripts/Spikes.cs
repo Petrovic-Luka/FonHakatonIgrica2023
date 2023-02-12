4da3a48b94b160c902c5aec879d7007dddfcab88
Petrovic-Luka <lukapetrovic00@gmail.com>
2023-02-12 12:39:07 +0100

    Napravio spike
    Nparavio spike prefab koji resetuje nivo ako igrac udari u njih

---
+++ Assets/_Scripts/Spikes.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spikes : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            int scene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
