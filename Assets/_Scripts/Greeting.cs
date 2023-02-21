4bfd4d69003f496eab6395257776c412fc516aa1
Petrovic-Luka <lukapetrovic00@gmail.com>
2023-02-21 20:38:20 +0100

    Olaksana verzija

--- Assets/_Scripts/Greeting.cs
+++ Assets/_Scripts/Greeting.cs
@@ -10,7 +10,6 @@ public class Greeting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 60;
         textGreeting.text="Zdravo "+ PlayerPrefs.GetString("name") +" ovo je Kevin pomozi mu da stigne do cilja i imaš šansu da osvojiš nagradu. Za ovo imaš 3 minuta a poene dobijaš za svaki kompletiran nivo, i to što brže više poena dobijaš. Vreme startuje kada klikneš na dugme.";
     }
 
